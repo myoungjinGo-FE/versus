ffb47942a376f40abbb35a286156abdfc74a7f7b
myoungjinGo-FE <cokemania2099@gmail.com>
2024-07-23 14:22:32 +0900

    initial commit

---
+++ src/main/java/com/versus/versus/VersusApplication.java
@@ -0,0 +1,13 @@
+package com.versus.versus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/versus/versus/VersusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.versus.versus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
